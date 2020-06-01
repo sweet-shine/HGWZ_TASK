afcc9805c70e22a54e69269f262b98f1518c07e0
wangc <932161889@qq.com>
2020-06-01 19:06:49 +0800

    APP第二次作业（增加查找元素的异常处理和弹框处理，增加删除成员页面（部分完成））

---
+++ APP/second_task/page/app.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+# auther:wangc
+# 2020-06-01
+from appium import webdriver
+
+from APP.second_task.page.base import BasePage
+from APP.second_task.page.main import MainPage
+
+
+class APP(BasePage):
+
+    def start_app(self):
+        if self._driver:
+            self._driver.launch_app()
+        else:
+            caps = {}
+            caps["platformName"] = "Android"
+            caps["deviceName"] = "127.0.0.1:7555"
+            caps["appPackage"] = "com.tencent.wework"
+            caps["appActivity"] = ".launch.LaunchSplashActivity"
+            caps['noReset'] = "true"
+            caps['skipServerInstallation'] = True
+            caps['skipDeviceInitialization'] = True
+            self._driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+
+        self._driver.implicitly_wait(5)
+
+        return self
+
+    def stop_app(self):
+        pass
+
+    def restart_app(self):
+        pass
+
+    def goto_main(self) -> MainPage:
+        return MainPage(self._driver)
--- APP/second_task/page/base.py
+++ APP/second_task/page/base.py
@@ -1,33 +1,48 @@
 # coding=utf-8
 # auther:wangc
 # 2020-05-26
-from appium import webdriver
+
 from appium.webdriver.common.mobileby import MobileBy
+from appium.webdriver.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 
 class BasePage:
-    _driver = None
+    _black_list = {'确定', '确认', '是', '下次再说'}
+    _max_retry = 3
+    _error_times = 0
 
-    def __init__(self, driver=None):
-        if driver:
-            self._driver = driver
-        else:
-            caps = {}
-            caps["platformName"] = "Android"
-            caps["deviceName"] = "127.0.0.1:7555"
-            caps["appPackage"] = "com.tencent.wework"
-            caps["appActivity"] = ".launch.LaunchSplashActivity"
-            caps['noReset'] = "true"
-            caps['skipServerInstallation'] = True
-            caps['skipDeviceInitialization'] = True
+    def __init__(self, driver: WebDriver = None):
+        self._driver = driver
 
-            self._driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+    def find_ele(self, locator, value: str = None):
+
+        try:
+            # 判断传进来的locator是否是元组，如果是解元组，如果不是，使用locator, value两个参数定位
+            if isinstance(locator, tuple):
+                element = self._driver.find_element(*locator)
+            else:
+                element = self._driver.find_element(locator, value)
+
             self._driver.implicitly_wait(5)
+            return element
 
-    def find_ele(self, by, locator):
-        return self._driver.find_element(by, locator)
+        # 如果未找到元素，则看看是否有需要处理的弹框
+        except Exception as e:
+            #每次找不到，错误次数就+1
+            self._error_times += 1
+            #如果错误次数>最大重试次数，抛出异常
+            if self._error_times > self._max_retry:
+                raise e
+            # 如果错误次数不够，则处理弹框
+            self._driver.implicitly_wait(1)
+            for _black_ele in self._black_list:
+                eles = self._driver.find_elements(MobileBy.XPATH, _black_ele)
+                if len(eles) > 0:
+                    eles[0].click()
+                    return self.find_ele(locator, value)
+            raise e
 
     def find_eles(self, by, locator):
         return self._driver.find_elements(by, locator)
